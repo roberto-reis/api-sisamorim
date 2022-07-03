72dc964fb97639a49173603f4e4cfdd4e111b318
Roberto Reis <tekinforroberto@gmail.com>
2022-07-03 15:34:27 -0300

    Alteração no docker-compose

---
+++ docker/app/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "########## COMPOSER INSTALL ##########"
+composer install
+
+echo "########## SETTING ENV ##########"
+cp .env.example .env
+
+echo "########## KEY GENERATE ##########"
+php artisan key:generate
+
+echo "########## RUNNING MIGRATIONS ##########"
+php artisan migrate
+
+php-fpm
