341b1d2a62ce56a376b39908c496ce181861a3c6
Roberto Reis <tekinforroberto@gmail.com>
2022-07-16 19:48:08 -0300

    SIS-40-chore: Criado a factory Centro de Custo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Domain\CentroCusto\Models\CentroCusto;
 use Illuminate\Database\Seeder;
 use App\Domain\User\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create();
+        // User::factory()->create();
+        // CentroCusto::factory()->create();
     }
 }
