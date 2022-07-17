6af42ecbee9cb3343020817cf9cc931be414f1d8
Roberto Reis <tekinforroberto@gmail.com>
2022-07-17 13:00:59 -0300

    SIS-40-test: test deve listar fornecedor

--- tests/Feature/Fornecedor/FornecedorTest.php
+++ tests/Feature/Fornecedor/FornecedorTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature\Fornecedor;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Domain\User\Models\User;
+use App\Domain\Fornecedor\Models\Fornecedor;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class FornecedorTest extends TestCase
 {
@@ -17,5 +19,59 @@ class FornecedorTest extends TestCase
         // Assert
         $response->assertJson(['error' => 'Authorization Token not found'])
                 ->assertStatus(401);
+    }
+
+    public function test_deve_listar_fornecedor()
+    {
+        // Arrange
+        $user = User::factory()->create([
+            'password' => bcrypt('Reis@12345678'),
+        ]);
+        Fornecedor::factory()->create();
+
+        // Action
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'Reis@12345678',
+        ]);
+        $token = $response->json('token')['access_token'];
+
+        $response = $this->withHeaders(['Authorization' => 'Bearer ' . $token])
+                         ->get(route('fornecedor.index'));
+        // Assert
+        $response->assertJsonStructure([
+            'data' => [
+                'data' => [
+                    0 => [
+                        'uuid',
+                        'nome_razao_social',
+                        'email',
+                        'cpf',
+                        'cnpj',
+                        'inscricao_estadual',
+                        'inscricao_municipal',
+                        'celular',
+                        'cep',
+                        'endereco',
+                        'numero',
+                        'complemento',
+                        'bairro',
+                        'cidade',
+                        'uf',
+                        'observacao',
+                        'tipo_fornecedor',
+                        'banco',
+                        'agencia',
+                        'digito_agencia',
+                        'conta',
+                        'digito_conta',
+                        'tipo_conta',
+                        'status',
+                        'created_at',
+                        'updated_at',
+                    ]
+                ]
+            ]
+        ])->assertStatus(200);
     }
 }
