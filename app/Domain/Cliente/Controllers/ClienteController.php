39eda16d1026c9f2b024339c992c3ec92ce6b3b3
Roberto Reis <tekinforroberto@gmail.com>
2022-08-06 16:28:03 -0300

    SIS-38-feat: Criando controller de Clientes

---
+++ app/Domain/Cliente/Controllers/ClienteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Cliente\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ClienteController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
