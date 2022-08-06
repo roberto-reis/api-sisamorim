1d3f3dabd25c931866d872e1248ea4804ce9a898
Roberto Reis <tekinforroberto@gmail.com>
2022-08-06 19:51:23 -0300

    SIS-38-chore: Criando o método store no controller de cliente

--- app/Domain/Cliente/Controllers/ClienteController.php
+++ app/Domain/Cliente/Controllers/ClienteController.php
@@ -2,13 +2,34 @@
 
 namespace App\Domain\Cliente\Controllers;
 
+use App\Domain\Cliente\Action\CreateClienteAction;
+use App\Domain\Cliente\Requests\ClienteRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Shared\DTO\Cliente\ClienteDTO;
 
 class ClienteController extends Controller
 {
     public function index()
     {
 
+    }
+
+    public function store(ClienteRequest $request, ClienteDTO $dto, CreateClienteAction $action)
+    {
+        try {
+            $dadosValidados = $dto->fromArray($request->validated());
+            $cliente = $action($dadosValidados);
+
+            return response()->json([
+                'message' => 'Cliente cadastrado com sucesso',
+                'data' => $cliente
+            ], 201);
+        } catch (\Exception $excepion) {
+            return response()->json([
+                'message' => 'Erro ao cadastrar cliente',
+                'data' => $excepion->getMessage()
+            ], 500);
+        }
     }
 }
