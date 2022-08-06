7a350a0bf14d70d34e48e8c2e994bc85d2acf5c2
Roberto Reis <tekinforroberto@gmail.com>
2022-08-06 19:47:29 -0300

    SIS-38-chore: Criando ClienteDTO

---
+++ app/Shared/DTO/Cliente/ClienteDTO.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Shared\DTO\Cliente;
+
+use App\Shared\DTO\DTOAbstract;
+
+class ClienteDTO extends DTOAbstract
+{
+    /** @var string */
+    public $nome;
+
+    /** @var ?string */
+    public $email;
+
+    /** @var ?string */
+    public $cpf_cnpj;
+
+    /** @var ?string */
+    public $rg;
+
+    /** @var ?string */
+    public $data_nascimento;
+
+    /** @var ?string */
+    public $celular;
+
+    /** @var ?string */
+    public $inscricao_estadual;
+
+    /** @var ?string */
+    public $inscricao_municipal;
+
+    /** @var ?string */
+    public $cep;
+
+    /** @var ?string */
+    public $endereco;
+
+    /** @var ?string */
+    public $numero;
+
+    /** @var ?string */
+    public $complemento;
+
+    /** @var ?string */
+    public $bairro;
+
+    /** @var ?string */
+    public $cidade;
+
+    /** @var ?string */
+    public $uf;
+
+    /** @var ?string */
+    public $observacao;
+
+    /** @var bool */
+    public $status;
+
+    public function register(
+        string $nome,
+        ?string $email,
+        ?string $cpf_cnpj,
+        ?string $rg,
+        ?string $dataNascimento,
+        ?string $celular,
+        ?string $inscricaoEstadual,
+        ?string $inscricaoMunicipal,
+        ?string $cep,
+        ?string $endereco,
+        ?string $numero,
+        ?string $complemento,
+        ?string $bairro,
+        ?string $cidade,
+        ?string $uf,
+        ?string $observacao,
+        bool $status
+    ): self {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->cpf_cnpj = $cpf_cnpj;
+        $this->rg = $rg;
+        $this->data_nascimento = $dataNascimento;
+        $this->celular = $celular;
+        $this->inscricao_estadual = $inscricaoEstadual;
+        $this->inscricao_municipal = $inscricaoMunicipal;
+        $this->cep = $cep;
+        $this->endereco = $endereco;
+        $this->numero = $numero;
+        $this->complemento = $complemento;
+        $this->bairro = $bairro;
+        $this->cidade = $cidade;
+        $this->uf = $uf;
+        $this->observacao = $observacao;
+        $this->status = $status;
+
+        return $this;
+    }
+
+}
