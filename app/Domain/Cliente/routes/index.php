e13c260eb8b961f18c1fa944acba09d5ef1421b4
Roberto Reis <tekinforroberto@gmail.com>
2022-08-06 16:28:38 -0300

    SIS-38-feat: Criando as rotas de clientes

---
+++ app/Domain/Cliente/routes/index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Domain\Cliente\routes;
+
+use Illuminate\Support\Facades\Route;
+use App\Domain\Cliente\Controllers\ClienteController;
+
+// Route::group(function () {
+
+// });
+
+Route::controller(ClienteController::class)->prefix('clientes')->group(function() {
+    Route::get('/', 'index')->name('clientes.index');
+    Route::post('/store', 'store') ->name('clientes.store');
+    Route::put('{uuid}/update', 'update')->name('clientes.update');
+    Route::delete('{uuid}/delete', 'delete')->name('clientes.delete');
+});
+
