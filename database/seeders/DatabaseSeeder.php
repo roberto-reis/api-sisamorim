42da3d1267f6c283dc8f90c0e15dc348d375b67a
Roberto Reis <tekinforroberto@gmail.com>
2022-08-07 12:28:07 -0300

    SIS-38-chore: Ajuste no DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Domain\User\Models\User;
-use App\Domain\Cliente\Models\Cliente;
-use App\Domain\Produto\Models\Produto;
-use App\Domain\Fornecedor\Models\Fornecedor;
-use App\Domain\CentroCusto\Models\CentroCusto;
+use App\Infrastructure\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory()->create();
-        Fornecedor::factory()->create();
-        Produto::factory()->create();
-        Cliente::factory()->create();
+        // Fornecedor::factory()->create();
+        // Produto::factory()->create();
+        // Cliente::factory()->create();
     }
 }
