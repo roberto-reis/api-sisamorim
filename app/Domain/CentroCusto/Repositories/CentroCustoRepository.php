0058baf0da339833a409ee7923124b5dbf0b5088
Roberto Reis <tekinforroberto@gmail.com>
2022-09-07 18:57:47 -0300

    SIS-45-chore: Ajuste para retornar produto com paginação

--- app/Domain/CentroCusto/Repositories/CentroCustoRepository.php
+++ app/Domain/CentroCusto/Repositories/CentroCustoRepository.php
@@ -4,6 +4,7 @@ namespace App\Domain\CentroCusto\Repositories;
 
 use Illuminate\Http\Request;
 use App\Infrastructure\Models\CentroCusto;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class CentroCustoRepository
@@ -15,15 +16,20 @@ class CentroCustoRepository
      * @param Request $request
      * @return LengthAwarePaginator
      */
-    public function getCentroCusto(Request $request): LengthAwarePaginator
+    public function getCentroCusto(Request $request): LengthAwarePaginator|Collection
     {
         // TODO: Mover para um Action
         $search = $request->get('search');
+        $withPaginate = (bool)$request->get('with_paginate', true);
         $this->perPage = $request->get('perPage', $this->perPage);
         $centroCustos = CentroCusto::query();
 
         if ($search) {
             $centroCustos->where('nome', 'like', "%{$search}%");
+        }
+
+        if ($withPaginate === false) {
+            return $centroCustos->get();
         }
 
         return $centroCustos->paginate($this->perPage);
